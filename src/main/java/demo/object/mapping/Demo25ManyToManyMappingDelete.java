9d63bf6ade8b6b64558160e5873258147daca716
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-09-12 19:04:16 +0300

    updated the mapping example and began working on delete example

--- src/main/java/demo/object/mapping/Demo25ManyToManyMappingDelete.java
+++ src/main/java/demo/object/mapping/Demo25ManyToManyMappingDelete.java
@@ -6,13 +6,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Demo24ManyToManyMapping {
+public class Demo25ManyToManyMappingDelete {
 
     public static void main(String[] args) {
-
 
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -21,8 +21,8 @@ public class Demo24ManyToManyMapping {
                 .buildSessionFactory();
 
         Session currentSession = sessionFactory.getCurrentSession();
-        currentSession.beginTransaction();
 
+        currentSession.beginTransaction();
 
         CourseV6 c1 = new CourseV6
                 .CourseV6Builder()
@@ -33,7 +33,6 @@ public class Demo24ManyToManyMapping {
                 .CourseV6Builder()
                 .setCourseTitle("Java master class")
                 .build();
-
 
         StudentV13 s1 = new StudentV13
                 .StudentV13Builder()
@@ -56,13 +55,24 @@ public class Demo24ManyToManyMapping {
         listStudentC2.add(s2);
         c2.setStudent(listStudentC2);
 
-
         currentSession.save(c1);
         currentSession.save(c2);
         currentSession.save(s1);
         currentSession.save(s2);
 
         currentSession.getTransaction().commit();
+
+        currentSession = sessionFactory.getCurrentSession();
+        currentSession.beginTransaction();
+
+
+        StudentV13 studentV13 = currentSession.get(StudentV13.class, 1l);
+
+        currentSession.delete(studentV13);
+
+
+        currentSession.getTransaction().commit();
+
         currentSession.close();
         sessionFactory.close();
 
